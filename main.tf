8528e5f03f117c2cc5f5b64f1f7857d38301dc8d
Moiz Khan <126510585+makgah@users.noreply.github.com>
2024-07-15 16:19:10 -0400

    add load balancer

--- main.tf
+++ main.tf
@@ -57,3 +57,38 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+
+  listeners = [
+    {
+      port = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  target_groups = [
+     {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        target_id = aws_instance.blog.id
+        port= 80
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
