4d0d64393cd996e10120b7bb8a165b9e424c2603
Moiz Khan <126510585+makgah@users.noreply.github.com>
2024-07-15 15:43:09 -0400

    close string

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog_new
+  name = "blog_new"
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
@@ -42,7 +42,7 @@ module "blog_sg" {
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-  
+
 }
 
 
