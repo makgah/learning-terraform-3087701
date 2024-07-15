8c4393e3ae4f53f2f0d2e110b5020e1990428962
Moiz Khan <126510585+makgah@users.noreply.github.com>
2024-07-15 15:42:36 -0400

    fix bug

--- main.tf
+++ main.tf
@@ -26,23 +26,15 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
 
-module "blog_sg"{
-  source = "terraform-aws-modules/security_group/aws"
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name = "blog_new
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow https and http in. Allow everything out"
-
-
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
@@ -51,6 +43,14 @@ resource "aws_security_group" "blog" {
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
   
+}
+
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow https and http in. Allow everything out"
+  vpc_id = data.aws_vpc.default.id
+
 }
 
 resource "aws_security_group_rule" "blog_http_in"{
